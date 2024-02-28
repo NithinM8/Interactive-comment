fe0942da4b123987714c8bcc49df332a5b84bd41
Nithin <nitiii6363@gmail.com>
2024-02-28 13:33:00 +0530

    initial setups

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import iconDelete from "./assets/images/image-amyrobson.png" 
+
+function App() {
+  return <div className="bg-black">
+    <img src={iconDelete }/>
+  </div>;
+}
+
+export default App;
